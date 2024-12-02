15b2fd929ad23a3a3c4639d8ec06470338f678b0
Immanuel W <l0calh34rtz@gmail.com>
2024-12-02 16:33:05 +0700

    feat: day 1 finished

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+// whitelist --> url yang dapat mengakses/me-request ke backend
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.metrodataacademy.id",
+];
+
+module.exports = allowedOrigins;
