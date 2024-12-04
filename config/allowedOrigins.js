ed62287ba7784323a8d10246b11c76a7f84750a6
Immanuel W <l0calh34rtz@gmail.com>
2024-12-04 15:42:32 +0700

    feat: add origin for port 5173

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,4 @@
 // whitelist --> url yang dapat mengakses/me-request ke backend
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.metrodataacademy.id",
-];
+const allowedOrigins = ["http://localhost:3000", "https://localhost:5173"];
 
 module.exports = allowedOrigins;
