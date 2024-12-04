de6fb986c197cdfd92bbba7fccd5f4072630067c
Immanuel W <l0calh34rtz@gmail.com>
2024-12-04 15:51:21 +0700

    fix: typo

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
 // whitelist --> url yang dapat mengakses/me-request ke backend
-const allowedOrigins = ["http://localhost:3000", "https://localhost:5173"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
 
 module.exports = allowedOrigins;
