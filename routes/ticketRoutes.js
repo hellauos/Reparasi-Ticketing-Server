3ab402c618a50981441734be30c0f3549e6f3ed5
Immanuel W <l0calh34rtz@gmail.com>
2024-12-06 13:51:22 +0700

    feat: add JWTVerifier

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(JWTVerifier);
+
 router.route("/").get().post().patch().delete();
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const JWTVerifier = require("../middleware/JWTVerifier");
+
+router.use(JWTVerifier);
 
 router
   .route("/")
